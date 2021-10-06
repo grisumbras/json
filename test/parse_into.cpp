040a84f2af752d786775f2a760aef539ed7cde7e
Peter Dimov <pdimov@gmail.com>
2021-10-06 22:02:48 +0300

    Add null_handler

--- test/parse_into.cpp
+++ test/parse_into.cpp
@@ -70,6 +70,11 @@ public:
         BOOST_TEST( !ec.failed() ) && BOOST_TEST( t1 == t2 );
     }
 
+    void testNull()
+    {
+        testParseInto( nullptr );
+    }
+
     void testBoolean()
     {
         testParseInto( false );
@@ -108,6 +113,8 @@ public:
 
     void testSequence()
     {
+        testParseInto<std::vector<std::nullptr_t>>( { nullptr, nullptr } );
+
         testParseInto< std::vector<bool> >( {} );
         testParseInto< std::vector<bool> >( { true, false } );
 
@@ -173,6 +180,7 @@ public:
 
     void run()
     {
+        testNull();
         testBoolean();
         testIntegral();
         testFloatingPoint();
